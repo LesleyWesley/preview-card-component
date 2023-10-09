73ccacc6e7059e745321ddfe78bfe948b7f042c8
lesley_w <lrwesley92@gmail.com>
2023-10-09 14:51:59 -0500

    Added all props  except color and mapped over data to render cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from "./components/Card.jsx"
+import data from "./data.js"
+
+console.log(data)
 
 function App() {
+
+  const cars = data.map(car => {
+    return (
+      <Card
+        key={car.carType}
+        car={car}
+      />
+    )
+  })
+
   return (
     <div className="container">
-      <Card />
-      <Card />
-      <Card />
+      {cars}
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import styled from "styled-components"
 
-function Card() {
+const StyledCard = styled.div`
+  background-color: ${ props => props.color}
+`;
+
+function Card(props) {
   return (
     <div className="card">
-      <img src="icon-sedans.svg" alt="sedan icon" />
-      <h2 className="card--title">Sedans</h2>
-      <p className="card--description">Choose a sedan for its affordability and excellent fuel economy.  Ideal for cruising in the city or on your next road trip.</p>
+      <img src={props.car.icon} alt="sedan icon" />
+      <h2 className="card--title">{props.car.carType}</h2>
+      <p className="card--description">{props.car.description}</p>
       <a href="#" className="card--link">Learn More</a>
     </div>
   )
