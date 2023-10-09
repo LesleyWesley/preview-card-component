60de88e77f31a815895926b91ae3feccfb633bb9
lesley_w <lrwesley92@gmail.com>
2023-10-09 16:29:27 -0500

    Added color styling in as props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,15 @@
 import React from "react"
-import styled from "styled-components"
-
-const StyledCard = styled.div`
-  background-color: ${ props => props.color}
-`;
 
 function Card(props) {
+    const color =props.car.color;
+    console.log(color);
+
   return (
-    <div className="card">
+    <div className="card" style={{backgroundColor: color}}>
       <img src={props.car.icon} alt="sedan icon" />
       <h2 className="card--title">{props.car.carType}</h2>
       <p className="card--description">{props.car.description}</p>
-      <a href="#" className="card--link">Learn More</a>
+      <a href="#" className="card--link" style={{color: color}}>Learn More</a>
     </div>
   )
 }
