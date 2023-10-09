228f1801ed4350cffdd5dcb74cceba52726b8877
lesley_w <lrwesley92@gmail.com>
2023-10-09 17:18:39 -0500

    Fixed color issues with hover on links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 
 function Card(props) {
-    const color =props.car.color;
-    console.log(color);
+  const color = props.car.color;
 
   return (
     <div className="card" style={{backgroundColor: color}}>
       <img src={props.car.icon} alt="sedan icon" />
       <h2 className="card--title">{props.car.carType}</h2>
       <p className="card--description">{props.car.description}</p>
-      <a href="#" className="card--link" style={{color: color}}>Learn More</a>
+      <a href="#" className={`card--link ${props.car.carType}`}>Learn More</a>
     </div>
   )
 }
